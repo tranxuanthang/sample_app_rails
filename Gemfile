11325a2c4c81fb5f06c1fe4a2181838a650f09d3
tranxuanthang <tranthang.yb@outlook.com>
2019-03-05 09:34:48 +0700

    Chapter 6: Modeling users (#3)
    * Chapter 6: Modeling users
    
    * Add config
    
    * Fix methods order in User model

--- Gemfile
+++ Gemfile
@@ -33,3 +33,5 @@ end
 
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem "bootstrap-sass", "3.3.7"
+gem "bcrypt"
+gem "config"
---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true, length: {maximum: Settings.max_email_length},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: Settings.min_password_length}
+  validates :name, presence: true, length: {maximum: Settings.max_name_length}
+  
+  before_save {self.email = email.downcase}
+
+  has_secure_password
+end
