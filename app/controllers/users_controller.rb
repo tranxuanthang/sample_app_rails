763ff25c5fb9fcc556856541434a653b6a5fd309
tranxuanthang <tranthang.yb@outlook.com>
2019-03-07 08:28:40 +0700

    Chapter 7: Sign up (#4)
    * Chapter 7: Sign up
    
    * Update .gitignore, fix indent, some other fixes...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find_by id: params[:id]
+    if !@user
+      flash[:danger] = I18n.t "user_not_found"
+    end
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = I18n.t "welcome_message"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit :name, :email, :password,
+        :password_confirmation
+    end
 end
